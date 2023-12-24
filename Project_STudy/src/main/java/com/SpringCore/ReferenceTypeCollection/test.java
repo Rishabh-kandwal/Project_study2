b9b9b13e5bba426a27fe7524f83adef4061ea542
Rishabh kandwal <rishabhkandwal2820@gmail.com>
2023-12-24 21:41:42 +0530

    OBJECT INJECTION

---
+++ Project_STudy/src/main/java/com/SpringCore/ReferenceTypeCollection/test.java
@@ -0,0 +1,16 @@
+package com.SpringCore.ReferenceTypeCollection;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class test {
+
+	public static void main(String[] args)
+	{
+		ApplicationContext context= new ClassPathXmlApplicationContext("com/SpringCore/ReferenceTypeCollection/Reference_config.xml");
+		A temp=(A) context.getBean("aref");
+		System.out.println(temp.getX());
+		System.out.println(temp.getOb().getY());
+	}
+
+}
