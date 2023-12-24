b922af1d3dd14a3b4240cf93a5d890d9f5525994
Rishabh kandwal <rishabhkandwal2820@gmail.com>
2023-12-24 11:08:52 +0530

    updated code 2

--- Project_STudy/src/main/java/com/cdac_PROJECT/Project_STudy/App.java
+++ Project_STudy/src/main/java/com/cdac_PROJECT/Project_STudy/App.java
@@ -1,6 +1,8 @@
 package com.cdac_PROJECT.Project_STudy;
 
 import java.io.File;
+import java.util.List;
+import java.util.Stack;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -9,14 +11,23 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        ApplicationContext context =new 
-ClassPathXmlApplicationContext("file:src/main/java/com/cdac_PROJECT/Project_STudy/config.xml");
-        bean1 b=(bean1) context.getBean("bean1");
-        System.out.println(b);
-    }
-}// test changes
+public class App {
+	public static void main(String[] args) {
+		try {
+
+			System.out.println("Hello World!");
+			ApplicationContext context = new ClassPathXmlApplicationContext(
+					"file:src/main/java/com/cdac_PROJECT/Project_STudy/config.xml");
+			bean1 b = (bean1) context.getBean("bean1");
+			System.out.println(b);
+			bean1 bb = (bean1) context.getBean("bean");
+			System.out.println(bb);
+			bean1 be = (bean1) context.getBean("be");
+			System.out.println(be);
+
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+}
+// test changes
--- Project_STudy/src/main/java/com/cdac_PROJECT/Project_STudy/bean1.java
+++ Project_STudy/src/main/java/com/cdac_PROJECT/Project_STudy/bean1.java
@@ -1,7 +1,8 @@
 package com.cdac_PROJECT.Project_STudy;
 
-public class bean1
-{
+import java.util.List;
+
+public class bean1 {
 	private int studentId;
 	private String studentName;
 	private String studentAddress;
@@ -9,41 +10,49 @@ public class bean1
 	public int getStudentId() {
 		return studentId;
 	}
+
 	public void setStudentId(int studentId) {
 		System.out.println("setting StudentId");
 		this.studentId = studentId;
 	}
-	public String getStudentName() {
 
+	public String getStudentName() {
+		System.out.println("getStudentName");
 		return studentName;
 	}
+
 	public void setStudentName(String studentName) {
+		System.out.println("setting setStudentName");
 		this.studentName = studentName;
 	}
+
 	public String getStudentAddress() {
+		System.out.println("getStudentAddress");
+
 		return studentAddress;
 	}
-	
+
 	public void setStudentAddress(String studentAddress) {
+		System.out.println("setting setStudentAddress");
 		this.studentAddress = studentAddress;
 	}
-	
-	
+
 	public bean1(int studentId, String studentName, String studentAddress) {
 		super();
 		this.studentId = studentId;
 		this.studentName = studentName;
 		this.studentAddress = studentAddress;
 	}
+
 	public bean1() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
+
 	@Override
 	public String toString() {
 		return "bean1 [studentId=" + studentId + ", studentName=" + studentName + ", studentAddress=" + studentAddress
 				+ "]";
 	}
-	
-	
+
 }
