b6f72c0d716d0f9c06b88960d9a74a2f78197b48
Rishabh kandwal <rishabhkandwal2820@gmail.com>
2023-12-28 23:50:22 +0530

    type="int" index="1"

--- Project_STudy/src/main/java/com/SpringCore/contructors/Customer.java
+++ Project_STudy/src/main/java/com/SpringCore/contructors/Customer.java
@@ -9,5 +9,10 @@ public class Customer {
 new ClassPathXmlApplicationContext("com/SpringCore/contructors/Person_config.xml");
 		Person p = (Person) context.getBean("person");
 		System.out.println(p);
+		System.out.println();
+		Adition a = (Adition) context.getBean("add");
+		a.dosum();
+		System.out.println(a);
+		
 	}
 }
